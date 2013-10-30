e72126d22a3ece06ce27a32c90ee89555aacf660
SteveAndrews76 <steve.andrews36@gmail.com>
2013-10-30 19:18:25 +1100

    animate markers, code alignment

--- mapData.js
+++ mapData.js
@@ -1,5 +1,4 @@
 // JavaScript Document
-// JavaScript Document
 var myCenter=new google.maps.LatLng(-37.97512392052657,145.2195382118225);
 var BuildA=new google.maps.LatLng(-37.97415,145.2175491231842);
 var BuildB=new google.maps.LatLng(-37.97415,145.2175491231842);
@@ -34,89 +33,90 @@ var ChisholmDandenong;
 function initialize()
 {
 	var imageBounds = new google.maps.LatLngBounds(
-      new google.maps.LatLng(-37.97668856261104, 145.21637320518494),
-      new google.maps.LatLng(-37.97396526056601, 145.22268176078796));
-	  //Bottom Left corner Co-ordinates are first. the Top Right Co-ordinates are second.
+		new google.maps.LatLng(-37.97668856261104, 145.21637320518494),
+		new google.maps.LatLng(-37.97396526056601, 145.22268176078796));
+		//Bottom Left corner Co-ordinates are first. the Top Right Co-ordinates are second.
 
 	
 	var mapProp = {
-	 center: myCenter,
- 	 zoom:17,
- 	 mapTypeId:google.maps.MapTypeId.ROADMAP
- 	 };
+		center: myCenter,
+		zoom:17,
+		mapTypeId:google.maps.MapTypeId.ROADMAP
+	};
  	//centering the map, setting how zoomed in and the type of map
  
  
-	 var map=new google.maps.Map(document.getElementById("Map")
-	  ,mapProp);
+	var map=new google.maps.Map(document.getElementById("Map"),mapProp);
   
-	  ChisholmDandenong = new google.maps.GroundOverlay(
- 	     'DandenongLayout2.png',
-	      imageBounds);
-	  ChisholmDandenong.setMap(map);
+	ChisholmDandenong = new google.maps.GroundOverlay('DandenongLayout2.png', imageBounds);
+	ChisholmDandenong.setMap(map);
 	// Adding the campus overlay
   
 	var markerDand=new google.maps.Marker({
-	  position:myCenter
-	  });
+		animation: google.maps.Animation.DROP,
+		position:myCenter
+	});
 	//Adding the marker
 	
 	markerDand.setMap(map);
 
 	var infowindow = new google.maps.InfoWindow({
-	  content:"Chisholm Dandenong",
-	  });
+		content:"Chisholm Dandenong",
+	});
 	//Adding text to the marker
 	
-	google.maps.event.addListener(markerDand, 'click', function() {
-	  infowindow.open(map,markerDand);
-	  });	  
+	google.maps.event.addListener(markerDand, 'click', function(){
+		infowindow.open(map,markerDand);
+	});
 	// event listener so that pop up bubble appears when marker is clicked
 	
 	//Building L
 	var markerL=new google.maps.Marker({
-	  position:BuildL,
-	  });
-	 
-	 markerL.setMap(map);
-	 
-	 var infowindowL = new google.maps.InfoWindow({
-	  content:"Building L",
-	  });
-	  
-	  google.maps.event.addListener(markerL, 'click', function() {
-	  infowindowL.open(map,markerL);
-	  });
+		position:BuildL,
+		animation: google.maps.Animation.DROP,
+	});
+	
+	markerL.setMap(map);
+	
+	var infowindowL = new google.maps.InfoWindow({
+		content:"Building L",
+	});
+	
+	google.maps.event.addListener(markerL, 'click', function() {
+		infowindowL.open(map,markerL);
+	});
 	
 	//Building V
 	var markerV=new google.maps.Marker({
-	  position:BuildV,
-	  });
-	 
-	 markerV.setMap(map);
-	 
-	 var infowindowV = new google.maps.InfoWindow({
-	  content:"Building V",
-	  });
-	  
-	  google.maps.event.addListener(markerV, 'click', function() {
-	  infowindowV.open(map,markerV);
-	  });
+		position:BuildV,
+		animation: google.maps.Animation.BOUNCE,
+	});
+	
+	markerV.setMap(map);
+	
+	var infowindowV = new google.maps.InfoWindow({
+		content:"Building V",
+	});
+	
+	google.maps.event.addListener(markerV, 'click', function() {
+		infowindowV.open(map,markerV);
+	});
 	
 	//Building W
 	var markerW=new google.maps.Marker({
-	  position:BuildW,
-	  });
-	 
-	 markerW.setMap(map);
-	 
-	 var infowindowW = new google.maps.InfoWindow({
-	  content:"Building W",
-	  });
-	  
-	  google.maps.event.addListener(markerW, 'click', function() {
-	  infowindowW.open(map,markerW);
-	  });
+		position:BuildW,
+		animation: google.maps.Animation.BOUNCE,
+	});
+	
+	markerW.setMap(map);
+	
+	var infowindowW = new google.maps.InfoWindow({
+		content:"Building W",
+	});
+	
+	google.maps.event.addListener(markerW, 'click', function() {
+		infowindowW.open(map,markerW);
+	});
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
