57ded9c4e8984c72d7008df440317b824b3eb8e5
SteveAndrews76 <steveandrews76@iprimus.com.au>
2013-11-14 23:29:29 +1100

    Display all markers
    For some reason the portable buildings (BuildPT1...) won't display, not
    sure why might have to explore if it's the number in the variable name
    or not ???

--- mapData.js
+++ mapData.js
@@ -23,10 +23,10 @@ var BuildU=new google.maps.LatLng(-37.97473065478011,145.2199673652649);
 var BuildV=new google.maps.LatLng(-37.97461648045906,145.21842777729034);
 var BuildW=new google.maps.LatLng(-37.97439235997906,145.21745681762695);
 var BuildY=new google.maps.LatLng(-37.975035118768,145.21964013576508);
-var BuildPt1=new google.maps.LatLng(-37.97563981433231,145.21886229515076);
-var BuildPt2=new google.maps.LatLng(-37.97563981433231,145.21900713443756);
-var BuildPt3=new google.maps.LatLng(-37.97565250019989,145.21915197372437);
-var BuildPt4=new google.maps.LatLng(-37.97504357608296,145.22158473730087);
+var BuildPT1=new google.maps.LatLng(-37.97563981433231,145.21886229515076);
+var BuildPT2=new google.maps.LatLng(-37.97563981433231,145.21900713443756);
+var BuildPT3=new google.maps.LatLng(-37.97565250019989,145.21915197372437);
+var BuildPT4=new google.maps.LatLng(-37.97504357608296,145.22158473730087);
 var ChisholmDandenong;
 
 function initialize()
@@ -40,7 +40,7 @@ function initialize()
 	var mapProp = {
 		center: myCenter,
 		zoom:17,
-		mapTypeId:google.maps.MapTypeId.ROADMAP
+		mapTypeId:google.maps.MapTypeId.HYBRID
 	};
  	//centering the map, setting how zoomed in and the type of map
  
@@ -70,7 +70,6 @@ function initialize()
 	// event listener so that pop up bubble appears when marker is clicked
 	
 	//Building A
-
 	var markerA=new google.maps.Marker({
 		position:BuildA,
 		animation: google.maps.Animation.DROP,
@@ -79,15 +78,14 @@ function initialize()
 	markerA.setMap(map);
 	
 	var infowindowA = new google.maps.InfoWindow({
-		content:"Building A",
+		content:"Building A"
 	});
 	
 	google.maps.event.addListener(markerA, 'click', function() {
-		infowindowA.open(map,markerL);
+		infowindowA.open(map,markerA);
 	});
 	
 	//Building B
-
 	var markerB=new google.maps.Marker({
 		position:BuildB,
 		animation: google.maps.Animation.DROP,
@@ -96,7 +94,7 @@ function initialize()
 	markerB.setMap(map);
 	
 	var infowindowB = new google.maps.InfoWindow({
-		content:"Building B",
+		content:"Building B"
 	});
 	
 	google.maps.event.addListener(markerB, 'click', function() {
@@ -104,7 +102,6 @@ function initialize()
 	});
 	
 	//Building C
-
 	var markerC=new google.maps.Marker({
 		position:BuildC,
 		animation: google.maps.Animation.DROP,
@@ -119,9 +116,56 @@ function initialize()
 	google.maps.event.addListener(markerC, 'click', function() {
 		infowindowC.open(map,markerC);
 	});
-
+	
+	//Building D
+	var markerD=new google.maps.Marker({
+		position:BuildD,
+		animation: google.maps.Animation.DROP,
+	});
+	
+	markerD.setMap(map);
+	
+	var infowindowD = new google.maps.InfoWindow({
+		content:"Building D",
+	});
+	
+	google.maps.event.addListener(markerD, 'click', function() {
+		infowindowD.open(map,markerD);
+	});
+	
+	//Building E
+	var markerE=new google.maps.Marker({
+		position:BuildE,
+		animation: google.maps.Animation.DROP,
+	});
+	
+	markerE.setMap(map);
+	
+	var infowindowE = new google.maps.InfoWindow({
+		content:"Building E",
+	});
+	
+	google.maps.event.addListener(markerE, 'click', function() {
+		infowindowE.open(map,markerE);
+	});
+	
+	//Building F
+	var markerF=new google.maps.Marker({
+		position:BuildF,
+		animation: google.maps.Animation.DROP,
+	});
+	
+	markerF.setMap(map);
+	
+	var infowindowF = new google.maps.InfoWindow({
+		content:"Building F",
+	});
+	
+	google.maps.event.addListener(markerF, 'click', function() {
+		infowindowF.open(map,markerF);
+	});
+	
 	//Building G
-
 	var markerG=new google.maps.Marker({
 		position:BuildG,
 		animation: google.maps.Animation.DROP,
@@ -137,6 +181,54 @@ function initialize()
 		infowindowG.open(map,markerG);
 	});
 	
+	//Building H
+	var markerH=new google.maps.Marker({
+		position:BuildH,
+		animation: google.maps.Animation.DROP,
+	});
+	
+	markerH.setMap(map);
+	
+	var infowindowH = new google.maps.InfoWindow({
+		content:"Building H",
+	});
+	
+	google.maps.event.addListener(markerH, 'click', function() {
+		infowindowH.open(map,markerH);
+	});
+	
+	//Building J
+	var markerJ=new google.maps.Marker({
+		position:BuildJ,
+		animation: google.maps.Animation.DROP,
+	});
+	
+	markerJ.setMap(map);
+	
+	var infowindowJ = new google.maps.InfoWindow({
+		content:"Building J",
+	});
+	
+	google.maps.event.addListener(markerJ, 'click', function() {
+		infowindowJ.open(map,markerJ);
+	});
+	
+	//Building K
+	var markerK=new google.maps.Marker({
+		position:BuildK,
+		animation: google.maps.Animation.DROP,
+	});
+	
+	markerK.setMap(map);
+	
+	var infowindowK = new google.maps.InfoWindow({
+		content:"Building K",
+	});
+	
+	google.maps.event.addListener(markerK, 'click', function() {
+		infowindowK.open(map,markerK);
+	});
+		
 	//Building L
 	var markerL=new google.maps.Marker({
 		position:BuildL,
@@ -153,8 +245,55 @@ function initialize()
 		infowindowL.open(map,markerL);
 	});
 	
+	//Building M
+	var markerM=new google.maps.Marker({
+		position:BuildM,
+		animation: google.maps.Animation.DROP,
+	});
+	
+	markerM.setMap(map);
+	
+	var infowindowM = new google.maps.InfoWindow({
+		content:"Building M",
+	});
+	
+	google.maps.event.addListener(markerM, 'click', function() {
+		infowindowM.open(map,markerM);
+	});
+	
+	//Building N
+	var markerN=new google.maps.Marker({
+		position:BuildN,
+		animation: google.maps.Animation.DROP,
+	});
+	
+	markerN.setMap(map);
+	
+	var infowindowN = new google.maps.InfoWindow({
+		content:"Building N",
+	});
+	
+	google.maps.event.addListener(markerN, 'click', function() {
+		infowindowN.open(map,markerN);
+	});
+	
+	//Building O
+	var markerO=new google.maps.Marker({
+		position:BuildO,
+		animation: google.maps.Animation.DROP,
+	});
+	
+	markerO.setMap(map);
+	
+	var infowindowO = new google.maps.InfoWindow({
+		content:"Building O",
+	});
+	
+	google.maps.event.addListener(markerO, 'click', function() {
+		infowindowO.open(map,markerO);
+	});
+	
 	//Building P
-
 	var markerP=new google.maps.Marker({
 		position:BuildP,
 		animation: google.maps.Animation.DROP,
@@ -168,6 +307,86 @@ function initialize()
 	
 	google.maps.event.addListener(markerP, 'click', function() {
 		infowindowP.open(map,markerP);
+	});
+	
+	//Building Q
+	var markerQ=new google.maps.Marker({
+		position:BuildQ,
+		animation: google.maps.Animation.DROP,
+	});
+	
+	markerQ.setMap(map);
+	
+	var infowindowQ = new google.maps.InfoWindow({
+		content:"Building Q",
+	});
+	
+	google.maps.event.addListener(markerQ, 'click', function() {
+		infowindowQ.open(map,markerQ);
+	});
+	
+	//Building R
+	var markerR=new google.maps.Marker({
+		position:BuildR,
+		animation: google.maps.Animation.DROP,
+	});
+	
+	markerR.setMap(map);
+	
+	var infowindowR = new google.maps.InfoWindow({
+		content:"Building R",
+	});
+	
+	google.maps.event.addListener(markerR, 'click', function() {
+		infowindowR.open(map,markerR);
+	});
+	
+	//Building S
+	var markerS=new google.maps.Marker({
+		position:BuildS,
+		animation: google.maps.Animation.DROP,
+	});
+	
+	markerS.setMap(map);
+	
+	var infowindowS = new google.maps.InfoWindow({
+		content:"Building S",
+	});
+	
+	google.maps.event.addListener(markerS, 'click', function() {
+		infowindowS.open(map,markerS);
+	});
+	
+	//Building T
+	var markerT=new google.maps.Marker({
+		position:BuildT,
+		animation: google.maps.Animation.DROP,
+	});
+	
+	markerT.setMap(map);
+	
+	var infowindowT = new google.maps.InfoWindow({
+		content:"Building T",
+	});
+	
+	google.maps.event.addListener(markerT, 'click', function() {
+		infowindowT.open(map,markerT);
+	});
+	
+	//Building U
+	var markerU=new google.maps.Marker({
+		position:BuildU,
+		animation: google.maps.Animation.DROP,
+	});
+	
+	markerU.setMap(map);
+	
+	var infowindowU = new google.maps.InfoWindow({
+		content:"Building U",
+	});
+	
+	google.maps.event.addListener(markerU, 'click', function() {
+		infowindowU.open(map,markerU);
 	});
 	
 	//Building V
@@ -201,6 +420,104 @@ function initialize()
 	google.maps.event.addListener(markerW, 'click', function() {
 		infowindowW.open(map,markerW);
 	});
+	
+	// Building X
+	var markerX=new google.maps.Marker({
+		position:BuildX,
+		animation: google.maps.Animation.DROP,
+	});
+	
+	markerX.setMap(map);
+	
+	var infowindowX = new google.maps.InfoWindow({
+		content:"Building X",
+	});
+	
+	google.maps.event.addListener(markerX, 'click', function() {
+		infowindowW.open(map,markerX);
+	});
+	
+	//Building Y
+	var markerY=new google.maps.Marker({
+		position:BuildY,
+		animation: google.maps.Animation.DROP,
+	});
+	
+	markerY.setMap(map);
+	
+	var infowindowY = new google.maps.InfoWindow({
+		content:"Building Y",
+	});
+	
+	google.maps.event.addListener(markerY, 'click', function() {
+		infowindowY.open(map,markerY);
+	});
+	
+	//Building PT1
+	var markerPT1=new google.maps.Marker({
+		position:BuildPT1,
+		animation: google.maps.Animation.DROP,
+	});
+	
+	markerPT1.setMap(map);
+	
+	var infowindowPT1 = new google.maps.InfoWindow({
+		content:"Building PT1",
+	});
+	
+	google.maps.event.addListener(markerPT1, 'click', function() {
+		infowindowPT1.open(map,markerPT1);
+	});
+	
+	//Building PT2
+	var markerPT2=new google.maps.Marker({
+		position:BuildPT2,
+		animation: google.maps.Animation.DROP,
+	});
+	
+	markerPT2.setMap(map);
+	
+	var infowindowPT2 = new google.maps.InfoWindow({
+		content:"Building PT2",
+	});
+	
+	google.maps.event.addListener(markerPT2, 'click', function() {
+		infowindowPT2.open(map,markerPT2);
+	});
+	
+	//Building PT3
+	var markerPT3=new google.maps.Marker({
+		position:BuildPT3,
+		animation: google.maps.Animation.DROP,
+	});
+	
+	markerPT3.setMap(map);
+	
+	var infowindowPT3 = new google.maps.InfoWindow({
+		content:"Building PT3",
+	});
+	
+	google.maps.event.addListener(markerPT3, 'click', function() {
+		infowindowPT3.open(map,markerPT3);
+	});
+	
+	//Building PT4
+	var markerPT4=new google.maps.Marker({
+		position:BuildPT4,
+		animation: google.maps.Animation.DROP,
+	});
+	
+	markerPT4.setMap(map);
+	
+	var infowindowPT4 = new google.maps.InfoWindow({
+		content:"Building PT4",
+	});
+	
+	google.maps.event.addListener(markerPT4, 'click', function() {
+		infowindowPT4.open(map,markerPT4);
+	});
+	
+	
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
